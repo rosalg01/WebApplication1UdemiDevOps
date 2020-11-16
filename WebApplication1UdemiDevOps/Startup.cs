ae43b98321469955a3343e47e1763e5258353c6e
Gilbert Rosal <gilbertrosal900@hotmail.com>
2020-11-16 15:47:09 -0500

    Simple change

--- WebApplication1UdemiDevOps/Startup.cs
+++ WebApplication1UdemiDevOps/Startup.cs
@@ -23,6 +23,7 @@ namespace WebApplication1UdemiDevOps
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Changes
             services.AddRazorPages();
         }
 
